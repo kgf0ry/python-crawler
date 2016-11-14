7a96aafa5f4b5edf1fca316494dbced1285d7d7f
kgf0ry <kgf0ry@gmail.com>
2016-11-14 13:25:41 +0800

    2016.11.14

--- rosiok2.py
+++ rosiok2.py
@@ -1,17 +1,17 @@
 #! usr/bin/env python
 #coding:utf-8
+import re
 import rosiok1
 import time
 import threading
 class rosiok:
     def __init__(self):
         self.url='http://www.rosiok.com'
-        self.ulist=['/shipin/','/app/','/x/','/shangjia/']
+        self.ulist=['/shipin/','/x/','/app/','/shangjia/']
         self.htmls=[]
         self.imgurls=[]
     def gethtmls(self):
-        i=self.ulist
-        i=i.pop()
+        i=self.ulist.pop()
         html=rosiok1.downhtml(self.url+i)
         while 1:
             self.htmls.append(html)
@@ -23,8 +23,12 @@ class rosiok:
             else:
                 print ('over.....')
                 break
-    def getimgs(self):
-        pass
+
+    def geturls(self):
+        while len(self.htmls):
+            
+            imgurl=re.findall('<li><a href="(.*?)" class="pimg"',self.htmls.pop())
+            self.imgurls.extend(imgurl) 
     def thread(self,fun):
         for i in range(len(self.ulist)):
             t=threading.Thread(target=fun)
@@ -34,7 +38,9 @@ class rosiok:
 
 if __name__=="__main__":
     R=rosiok()
-    R.thread(R.gethtmls)
-    print R.ulist
-
-    print ('over') 
+    #R.thread(R.gethtmls)
+    R.gethtmls()
+    R.gethtmls()
+    print (len(R.htmls))
+    R.thread(R.geturls())
+    print R.imgurls
